9b97c9e370ac93f2d19dad4bdcffaa896c0db809
Jun Jian Shen <jshen9085@gmail.com>
2019-02-26 21:32:26 -0500

    add method

--- Bakery/src/com/company/Tray.java
+++ Bakery/src/com/company/Tray.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Collections;
 
 public class Tray {
     private int capacity;
@@ -8,8 +9,23 @@ public class Tray {
 
     public Tray() {
         this.capacity = 10;
-        this.tray = new LinkedList<Cupcake>();
+        this.tray = new LinkedList<>();
     }
 
-    
+    public Cupcake addOneCupcake(String cupcakeType) throws IllegalStateException{
+        while(true){
+            synchronized (tray){
+                if(tray.size() >= this.capacity){
+                    tray.wait();
+                    continue;
+                }
+                Cupcake cupcake = new Cupcake(cupcakeType);
+                tray.add(cupcake);
+                System.out.println("Cupcake added " + cupcake);
+                return cupcake;
+            }
+        }
+        throw new IllegalStateException("Could not add cupcake to tray.");
+    }
+
 }
